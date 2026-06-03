da07d2db59c1a660e98c2096d9674b56d9b51abb
fallstall <kata.vershinina@gmail.com>
2026-06-03 11:23:47 +0300

    Initial commit

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Logo from '@/components/ui/Logo.vue';
+import Emblem from '@/components/header/emblem.vue';
+
+
+</script>
+
+<template>
+  <Logo />
+
+  <Emblem />
+  
+  <div>
+    <span>Производитель турецкого </span>
+  </div>
+</template>
+
+<style>
+
+</style>
