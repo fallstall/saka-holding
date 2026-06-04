b5e759e6307f00306958a23b1493db77217109a0
fallstall <kata.vershinina@gmail.com>
2026-06-04 13:43:53 +0300

    Update header button

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Logo from '@/components/ui/Logo.vue';
 import Emblem from '@/components/header/emblem.vue';
+import Button from '@/components/ui/Button.vue';
 
 
 </script>
@@ -13,6 +14,8 @@ import Emblem from '@/components/header/emblem.vue';
   <div>
     <span>Производитель турецкого </span>
   </div>
+
+  <Button theme="only-text" text="afaf"/>
 </template>
 
 <style>
--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
 
+defineProps<{
+    text: string
+    theme: 'full' | 'icon' | 'only-text'
+    transform: string
+}>()
+
 </script>
 
 <template>
-  
+    <button class="'btn-${{theme}}'">
+        <span v-if="theme !== 'icon'">{{ text }}</span>
+        <img v-if="theme === 'icon' || theme === 'full'" src="" alt="">
+    </button>
 </template>
 
-<style>
+<style scoped lang="scss">
+.btn {
 
+
+    
+    &-full {
+
+    }
+    &-icon {
+
+    }
+    &-only-text {
+
+    }
+
+}
 </style>
