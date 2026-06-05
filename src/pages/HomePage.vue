270447b5550d926a03f97b358a2bba1bff25399f
daravahonina35543-cloud <daravahonina35543@gmail.com>
2026-06-05 14:05:01 +0300

    добавила

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
     import Subtitle from '@/components/home/Subtitle.vue';
     import Slider from '@/components/home/Slider.vue';
+    import NewsCards from '../components/ui/news-cards/News-cards.vue'; 
 </script>
 <template>
 
 <Subtitle /> 
 <Slider />
-
+<NewsCards />
 
 
 
