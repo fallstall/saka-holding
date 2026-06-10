5defe2bcfbd35282239b6798c2c20912e7adb35b
daravahonina35543-cloud <daravahonina35543@gmail.com>
2026-06-10 10:39:12 +0300

    lalal

--- src/pages/CatalogPage.vue
+++ src/pages/CatalogPage.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import BottomBlockFull from '@/components/catalog/BottomBlockFull.vue'
+import SubtitlePage from '../components/SubtitlePage.vue';
+import TitlePage from '../components/TitlePage.vue';
 
 </script>
-
+ 
 <template>
+    <div class="cat-title">
+        <SubtitlePage subtitle="ГЛАВНАЯ"/>
+        <TitlePage title="КАТАЛОГ"/>
+    </div>
     <div class="catalog-page">
         <BottomBlockFull />
     </div>
@@ -11,5 +17,10 @@ import BottomBlockFull from '@/components/catalog/BottomBlockFull.vue'
 <style scoped lang="scss">
     .catalog-page {
         padding-left: 170px;
+    }
+    .cat-title{
+        padding-left: 170px;
+        padding-top: 100px;
+        padding-bottom: 50px;
     }
 </style>
